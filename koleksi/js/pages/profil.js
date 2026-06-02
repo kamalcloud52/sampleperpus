c2a45ecede2990e6ace98a15e11ab1894fb13c93
Kamal Fikar <amakita150@gmail.com>
2026-06-02 14:52:02 +0700

    Update profil.js

--- koleksi/js/pages/profil.js
+++ koleksi/js/pages/profil.js
@@ -2,18 +2,7 @@
 import { openModal } from '../components/modal.js';
 
 export function renderProfil(main) {
-    // Redirect ke modal profil
     setTimeout(() => {
         openModal('modalProfil');
-        
-        // Kembali ke home setelah modal ditutup
-        const modalProfil = document.getElementById('modalProfil');
-        const observer = new MutationObserver(() => {
-            if (!modalProfil.classList.contains('open')) {
-                window.location.hash = '#/home';
-                observer.disconnect();
-            }
-        });
-        observer.observe(modalProfil, { attributes: true, attributeFilter: ['class'] });
     }, 100);
 }
