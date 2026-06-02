98fd3e29116d37b8bf9fb58d2b26a78365adbfaa
Kamal Fikar <amakita150@gmail.com>
2026-06-02 14:52:34 +0700

    Update filter.js

--- koleksi/js/pages/filter.js
+++ koleksi/js/pages/filter.js
@@ -2,18 +2,7 @@
 import { openModal } from '../components/modal.js';
 
 export function renderFilter(main) {
-    // Redirect ke modal filter
     setTimeout(() => {
         openModal('modalFilter');
-        
-        // Kembali ke home setelah modal ditutup
-        const modalFilter = document.getElementById('modalFilter');
-        const observer = new MutationObserver(() => {
-            if (!modalFilter.classList.contains('open')) {
-                window.location.hash = '#/home';
-                observer.disconnect();
-            }
-        });
-        observer.observe(modalFilter, { attributes: true, attributeFilter: ['class'] });
     }, 100);
 }
